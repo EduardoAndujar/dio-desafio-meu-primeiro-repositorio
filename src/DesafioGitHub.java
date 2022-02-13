211c4cc3e1c9ea628af84c8f1bf615aa9f2435a3
EduardoAndujar <educasarotto@gmail.com>
2022-02-13 17:54:19 -0300

    first commit

---
+++ src/DesafioGitHub.java
@@ -0,0 +1,12 @@
+
+public class DesafioGitHub {
+
+	public static void main(String[] args) {
+
+		System.out.println("Meu primeiro reposiório GitHub");
+		System.out.println("Entrega do desafio da DIO!!!!");
+		System.out.println(":)");
+		
+	}
+
+}
